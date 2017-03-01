4c3edbceece18053d45cf83d87139042b3b3ad16
yannick-kouakam <y.kouakamkuegue@innopolis.ru>
2017-03-01 16:39:34 +0300

    success

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ express()
  .get("*", function(req, res) {
     db("users").then(function(data) {
       res.send(data);
+      res.send("Continious integration and deployment lab finished");
     });
  })
  .listen(80, function(err) {
