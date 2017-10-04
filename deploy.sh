90a6916cd3f0a25bc868062b1eef6b967d417afa
Artur Khayaliev <a.khayaliev@innopolis.ru>
2017-10-04 13:45:02 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-docker build -t kostyaurysov/sample-node .
-docker push kostyaurysov/sample-node
+docker build -t zytfo/sample-node .
+docker push zytfo/sample-node
 
 ssh deploy@35.187.30.81 << EOF
-docker pull kostyaurysov/sample-node:latest
+docker pull zytfo/sample-node:latest
 docker stop web || true
 docker rm web || true
-docker rmi kostyaurysov/sample-node:current || true
-docker tag kostyaurysov/sample-node:latest kostyaurysov/sample-node:current
-docker run -d --net app --restart always --name web -p 80:80 kostyaurysov/sample-node:current
+docker rmi zytfo/sample-node:current || true
+docker tag zytfo/sample-node:latest zytfo/sample-node:current
+docker run -d --net app --restart always --name web -p 80:80 zytfo/sample-node:current
 EOF
