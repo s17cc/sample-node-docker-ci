95e22c8719203a9670fbf723e3d759e948603cb0
Khanda Kevin <kevin.khanda@gmail.com>
2017-10-20 19:14:41 +0300

    Small fixes

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 docker push kkhanda/sample-node
 
-ssh deploy@35.195.193.222 << EOF
+ssh kevin_khanda@35.195.193.222 << EOF
 docker pull kkhanda/sample-node:latest
 docker stop web || true
 docker rm web || true
 docker rmi kkhanda/sample-node:current || true
 docker tag kkhanda/sample-node:latest kkhanda/sample-node:current
-docker run -d --net app --restart always --name web -p 80:80 kkhanda/sample-node:current
+docker run -d --restart always --name web -p 80:80 kkhanda/sample-node:current
 EOF
